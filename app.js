a97f14ec22409d497efdc8594a38a703f6e276c5
sergiuHudrea <sergiu.hudrea@kpmg.co.uk>
2022-12-12 14:48:54 +0000

    finish 3 GET /api/categories

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const { getCategories } = require('./controllers/games');
+
+
+
+app.get('/api/categories', getCategories);
+
+
+app.use((err, req,  res, next) => {
+    if (err.code === '22P02') {
+        res.status(400).send( {msg: "Bad Request."} );
+    } else {
+        next(err);
+    }
+});
+
+app.use((err, req,  res, next) => {
+    if (err.msg !== undefined) {
+        res.status(err.status).send( {msg: err.msg} );
+    } else {
+        next(err);
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send( { msg: "Internal Server Error."} );
+})
+
+module.exports = app;
