0ea1a41cac0f27720faf98686e941af951a97242
sergiuHudrea <sergiu.hudrea@kpmg.co.uk>
2022-12-16 12:30:06 +0000

    modified connection

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,14 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+const config = 
+        ENV === 'production'
+          ? {
+                connectionString: process.env.DATABASE_URL,
+                max: 2,
+            }
+          : {};
+
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -9,12 +17,6 @@ if (!process.env.PGDATABASE &&  !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const config = 
-        ENV === 'production'
-          ? {
-                connectionString: process.env.DATABASE_URL,
-                max: 2,
-            }
-          : {};
+
 
 module.exports = new Pool(config);
