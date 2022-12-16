264c8b01529ae1ae77d59242854b69464cef9de4
sergiuHudrea <sergiu.hudrea@kpmg.co.uk>
2022-12-16 13:00:32 +0000

    the Elephant works

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,16 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+
+if (!process.env.PGDATABASE &&  !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
 const config = 
         ENV === 'production'
           ? {
@@ -8,15 +18,5 @@ const config =
                 max: 2,
             }
           : {};
-
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
-if (!process.env.PGDATABASE &&  !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
-
-
 
 module.exports = new Pool(config);
